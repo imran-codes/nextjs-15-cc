7737d12bdfa675ef70fdcf268640e32608820413
imranh481 <imranh160@gmail.com>
2025-03-23 16:32:40 +0000

    added in headers to next config to fix cors issues

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,29 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["images.unsplash.com", "plus.unsplash.com"],
   },
+  async headers() {
+    return [
+      {
+        source: "/api/**", // Specific to /api
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://nextjs-15-cc-eight.vercel.app/", // Allow only your frontend
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
